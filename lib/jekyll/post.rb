e4f28cb075471b5418c6d2db498b803e64176e1e
mreid <mark@reid.name>
2010-08-09 22:11:40 +1000

    Added new single file render using site layout

--- lib/jekyll/post.rb
+++ lib/jekyll/post.rb
@@ -3,13 +3,13 @@ module Jekyll
   class Post
     include Comparable
     include Convertible
-        
+
     class << self
       attr_accessor :lsi
     end
-    
+
     MATCHER = /^(.+\/)*(\d+-\d+-\d+)-(.*)(\.[^.]+)$/
-    
+
     # Post name validator. Post filenames must be like:
     #   2008-11-05-my-awesome-post.textile
     #
@@ -17,36 +17,75 @@ module Jekyll
     def self.valid?(name)
       name =~ MATCHER
     end
-    
-    attr_accessor :date, :slug, :ext, :categories, :topics
-    attr_accessor :data, :content, :output
-    
+
+    attr_accessor :site, :date, :slug, :ext, :published, :data, :content, :output, :tags
+    attr_writer :categories
+
+    def categories
+      @categories ||= []
+    end
+
     # Initialize this Post instance.
+    #   +site+ is the Site
     #   +base+ is the String path to the dir containing the post file
     #   +name+ is the String filename of the post file
     #   +categories+ is an Array of Strings for the categories for this post
     #
     # Returns <Post>
-    def initialize(source, dir, name)
-      @base = File.join(source, dir, '_posts')
+    def initialize(site, source, dir, name, single=false)
+      @site = site
       @name = name
-      
-      self.categories = dir.split('/').reject { |x| x.empty? }
-      
-      parts = name.split('/')
-      self.topics = parts.size > 1 ? parts[0..-2] : []
-      
+      @single = single
+      if single
+        @base = File.join(source, dir)
+      else
+        @base = File.join(source, dir, '_posts')
+      end
+
+      self.categories = dir.split('/').reject { |x| x.empty? } unless @single
       self.process(name)
       self.read_yaml(@base, name)
+
+      if self.data.has_key?('published') && self.data['published'] == false
+        self.published = false
+      else
+        self.published = true
+      end
+
+      if self.data.has_key?("tag")
+        self.tags = [self.data["tag"]]
+      elsif self.data.has_key?("tags")
+        self.tags = self.data['tags']
+      else
+        self.tags = []
+      end
+
+      if self.categories.empty?
+        if self.data.has_key?('category')
+          self.categories << self.data['category']
+        elsif self.data.has_key?('categories')
+          # Look for categories in the YAML-header, either specified as
+          # an array or a string.
+          if self.data['categories'].kind_of? String
+            self.categories = self.data['categories'].split
+          else
+            self.categories = self.data['categories']
+          end
+        end
+      end
     end
-    
-    # Spaceship is based on Post#date
+
+    # Spaceship is based on Post#date, slug
     #
     # Returns -1, 0, 1
     def <=>(other)
-      self.date <=> other.date
+      cmp = self.date <=> other.date
+      if 0 == cmp
+       cmp = self.slug <=> other.slug
+      end
+      return cmp
     end
-    
+
     # Extract information from the post filename
     #   +name+ is the String filename of the post file
     #
@@ -57,7 +96,7 @@ module Jekyll
       self.slug = slug
       self.ext = ext
     end
-    
+
     # The generated directory into which the post will be placed
     # upon generation. This is derived from the permalink or, if
     # permalink is absent, set to the default date
@@ -65,18 +104,9 @@ module Jekyll
     #
     # Returns <String>
     def dir
-      if permalink
-        permalink.to_s.split("/")[0..-2].join("/")
-      else
-        prefix = self.categories.empty? ? '' : '/' + self.categories.join('/')
-        if Jekyll.permalink_style == :date
-          prefix + date.strftime("/%Y/%m/%d/")
-        else
-          prefix + '/'
-        end
-      end
+      File.dirname(url)
     end
-    
+
     # The full path and filename of the post.
     # Defined in the YAML of the post body
     # (Optional)
@@ -85,30 +115,53 @@ module Jekyll
     def permalink
       self.data && self.data['permalink']
     end
-    
+
+    def template
+      case self.site.permalink_style
+      when :pretty
+        "/:categories/:year/:month/:day/:title/"
+      when :none
+        "/:categories/:title.html"
+      when :date
+        "/:categories/:year/:month/:day/:title.html"
+      else
+        self.site.permalink_style.to_s
+      end
+    end
+
     # The generated relative url of this post
     # e.g. /2008/11/05/my-awesome-post.html
     #
     # Returns <String>
     def url
-      self.dir + self.slug + ".html"
+      return permalink if permalink
+
+      @url ||= {
+        "year"       => date.strftime("%Y"),
+        "month"      => date.strftime("%m"),
+        "day"        => date.strftime("%d"),
+        "title"      => CGI.escape(slug),
+        "categories" => categories.sort.join('/')
+      }.inject(template) { |result, token|
+        result.gsub(/:#{token.first}/, token.last)
+      }.gsub(/\/\//, "/")
     end
-    
+
     # The UID for this post (useful in feeds)
     # e.g. /2008/11/05/my-awesome-post
     #
     # Returns <String>
     def id
-      self.dir + self.slug
+      File.join(self.dir, self.slug)
     end
-        
+
     # Calculate related posts.
     #
     # Returns [<Post>]
     def related_posts(posts)
       return [] unless posts.size > 1
-      
-      if Jekyll.lsi
+
+      if self.site.lsi
         self.class.lsi ||= begin
           puts "Running the classifier... this could take a while."
           lsi = Classifier::LSI.new
@@ -123,7 +176,7 @@ module Jekyll
         (posts - [self])[0..9]
       end
     end
-    
+
     # Add any necessary layouts to this post
     #   +layouts+ is a Hash of {"name" => "layout"}
     #   +site_payload+ is the site payload hash
@@ -137,37 +190,66 @@ module Jekyll
         "page" => self.to_liquid
       }
       payload = payload.deep_merge(site_payload)
-      
+
       do_layout(payload, layouts)
     end
-    
+
     # Write the generated post file to the destination directory.
     #   +dest+ is the String path to the destination dir
     #
     # Returns nothing
     def write(dest)
       FileUtils.mkdir_p(File.join(dest, dir))
-      
-      path = File.join(dest, self.url)
+
+      # The url needs to be unescaped in order to preserve the correct filename
+      path = File.join(dest, CGI.unescape(self.url))
+
+      if template[/\.html$/].nil?
+        FileUtils.mkdir_p(path)
+        path = File.join(path, "index.html")
+      end
+
       File.open(path, 'w') do |f|
         f.write(self.output)
       end
     end
-    
+
     # Convert this post into a Hash for use in Liquid templates.
     #
     # Returns <Hash>
     def to_liquid
-      { "title" => self.data["title"] || self.slug.split('-').select {|w| w.capitalize! || w }.join(' '),
-        "url" => self.url,
-        "date" => self.date,
-        "id" => self.id,
-        "topics" => self.topics,
-        "content" => self.content }.deep_merge(self.data)
+      { "title"      => self.data["title"] || self.slug.split('-').select {|w| w.capitalize! || w }.join(' '),
+        "url"        => self.url,
+        "date"       => self.date,
+        "id"         => self.id,
+        "categories" => self.categories,
+        "next"       => self.next,
+        "previous"   => self.previous,
+        "tags"       => self.tags,
+        "content"    => self.content }.deep_merge(self.data)
     end
-    
+
     def inspect
       "<Post: #{self.id}>"
+    end
+
+    def next
+      pos = self.site.posts.index(self)
+
+      if pos && pos < self.site.posts.length-1
+        self.site.posts[pos+1]
+      else
+        nil
+      end
+    end
+
+    def previous
+      pos = self.site.posts.index(self)
+      if pos && pos > 0
+        self.site.posts[pos-1]
+      else
+        nil
+      end
     end
   end
 
--- lib/jekyll/site.rb
+++ lib/jekyll/site.rb
@@ -1,82 +1,171 @@
 module Jekyll
-  
+
   class Site
-    attr_accessor :config, :layouts, :posts
-    
+    attr_accessor :config, :layouts, :posts, :categories, :exclude,
+                  :source, :dest, :lsi, :pygments, :permalink_style, :tags
+
     # Initialize the site
     #   +config+ is a Hash containing site configurations details
     #
     # Returns <Site>
     def initialize(config)
-      self.config = config.clone
-      self.layouts = {}
-      self.posts = []
+      self.config          = config.clone
+
+      self.source          = config['source']
+      self.dest            = config['destination']
+      self.lsi             = config['lsi']
+      self.pygments        = config['pygments']
+      self.permalink_style = config['permalink'].to_sym
+      self.exclude         = config['exclude'] || []
+
+      self.reset
+      self.setup
     end
-    
-    # The directory containing the proto-site.
-    def source; self.config['source']; end
-    
-    # Where the completed site should be written.
-    def dest; self.config['destination']; end
-    
+
+    def reset
+      self.layouts         = {}
+      self.posts           = []
+      self.categories      = Hash.new { |hash, key| hash[key] = [] }
+      self.tags            = Hash.new { |hash, key| hash[key] = [] }
+    end
+
+    def setup
+      # Check to see if LSI is enabled.
+      require 'classifier' if self.lsi
+
+      # Set the Markdown interpreter (and Maruku self.config, if necessary)
+      case self.config['markdown']
+        when 'rdiscount'
+          begin
+            require 'rdiscount'
+
+            def markdown(content)
+              RDiscount.new(content).to_html
+            end
+
+          rescue LoadError
+            puts 'You must have the rdiscount gem installed first'
+          end
+        when 'maruku'
+          begin
+            require 'maruku'
+
+            def markdown(content)
+              Maruku.new(content).to_html
+            end
+
+            if self.config['maruku']['use_divs']
+              require 'maruku/ext/div'
+              puts 'Maruku: Using extended syntax for div elements.'
+            end
+
+            if self.config['maruku']['use_tex']
+              require 'maruku/ext/math'
+              puts "Maruku: Using LaTeX extension. Images in `#{self.config['maruku']['png_dir']}`."
+
+              # Switch off MathML output
+              MaRuKu::Globals[:html_math_output_mathml] = false
+              MaRuKu::Globals[:html_math_engine] = 'none'
+
+              # Turn on math to PNG support with blahtex
+              # Resulting PNGs stored in `images/latex`
+              MaRuKu::Globals[:html_math_output_png] = true
+              MaRuKu::Globals[:html_png_engine] =  self.config['maruku']['png_engine']
+              MaRuKu::Globals[:html_png_dir] = self.config['maruku']['png_dir']
+              MaRuKu::Globals[:html_png_url] = self.config['maruku']['png_url']
+            end
+          rescue LoadError
+            puts "The maruku gem is required for markdown support!"
+          end
+        else
+          raise "Invalid Markdown processor: '#{self.config['markdown']}' -- did you mean 'maruku' or 'rdiscount'?"
+      end
+    end
+
+    def textile(content)
+      RedCloth.new(content).to_html
+    end
+
     # Do the actual work of processing the site and generating the
     # real deal.
     #
     # Returns nothing
     def process
+      self.reset
       self.read_layouts
       self.transform_pages
       self.write_posts
     end
-    
-    # Read all the files in <source>/_layouts except backup files
-    # (end with "~") into memory for later use.
+
+    # Process a single file using the site's configuration and layouts. 
+    #
+    # Returns nothing
+    def process_single(filename)
+      self.reset
+      self.read_layouts
+      dir = File.dirname(filename)
+      f = File.basename(filename)
+      post = Post.new(self, self.source, dir, f, true)
+
+      if post.published
+        self.posts << post
+        #post.categories.each { |c| self.categories[c] << post }
+        #post.tags.each { |c| self.tags[c] << post }
+      end   
+      post.render(self.layouts, site_payload)
+      post.write(File.join(self.dest, dir))
+    end
+
+    # Read all the files in <source>/_layouts into memory for later use.
     #
     # Returns nothing
     def read_layouts
       base = File.join(self.source, "_layouts")
-      entries = Dir.entries(base)
-      entries = entries.reject { |e| e[-1..-1] == '~' }
-      entries = entries.reject { |e| File.directory?(File.join(base, e)) }
-      
+      entries = []
+      Dir.chdir(base) { entries = filter_entries(Dir['*.*']) }
+
       entries.each do |f|
         name = f.split(".")[0..-2].join(".")
-        self.layouts[name] = Layout.new(base, f)
+        self.layouts[name] = Layout.new(self, base, f)
       end
     rescue Errno::ENOENT => e
       # ignore missing layout dir
     end
-    
-    # Read all the files in <base>/_posts except backup files (end with "~")
-    # and create a new Post object with each one.
+
+    # Read all the files in <base>/_posts and create a new Post object with each one.
     #
     # Returns nothing
     def read_posts(dir)
       base = File.join(self.source, dir, '_posts')
-      
       entries = []
-      Dir.chdir(base) { entries = Dir['**/*'] }
-      entries = entries.reject { |e| e[-1..-1] == '~' }
-      entries = entries.reject { |e| File.directory?(File.join(base, e)) }
+      Dir.chdir(base) { entries = filter_entries(Dir['**/*']) }
 
       # first pass processes, but does not yet render post content
       entries.each do |f|
         if Post.valid?(f)
-          post = Post.new(self.source, dir, f)
-          self.posts << post
+          post = Post.new(self, self.source, dir, f)
+
+          if post.published
+            self.posts << post
+            post.categories.each { |c| self.categories[c] << post }
+            post.tags.each { |c| self.tags[c] << post }
+          end
         end
       end
-      
+
+      self.posts.sort!
+
       # second pass renders each post now that full site payload is available
       self.posts.each do |post|
         post.render(self.layouts, site_payload)
       end
-      
-      self.posts.sort!
+
+      self.categories.values.map { |ps| ps.sort! { |a, b| b <=> a} }
+      self.tags.values.map { |ps| ps.sort! { |a, b| b <=> a} }
     rescue Errno::ENOENT => e
       # ignore missing layout dir
     end
-    
+
     # Write each post to <dest>/<year>/<month>/<day>/<slug>
     #
     # Returns nothing
@@ -85,46 +174,49 @@ module Jekyll
         post.write(self.dest)
       end
     end
-    
+
     # Copy all regular files from <source> to <dest>/ ignoring
     # any files/directories that are hidden or backup files (start
-    # with "." or end with "~") or contain site content (start with "_")
-    # unless they are "_posts" directories
+    # with "." or "#" or end with "~") or contain site content (start with "_")
+    # unless they are "_posts" directories or web server files such as
+    # '.htaccess'
     #   The +dir+ String is a relative path used to call this method
     #            recursively as it descends through directories
     #
     # Returns nothing
     def transform_pages(dir = '')
       base = File.join(self.source, dir)
-      entries = Dir.entries(base)
-      entries = entries.reject { |e| e[-1..-1] == '~' }
-      entries = entries.reject do |e|
-        (e != '_posts') and ['.', '_'].include?(e[0..0])
-      end
+      entries = filter_entries(Dir.entries(base))
+      directories = entries.select { |e| File.directory?(File.join(base, e)) }
+      files = entries.reject { |e| File.directory?(File.join(base, e)) }
 
-      # we need to make sure to process _posts *first* otherwise they 
+      # we need to make sure to process _posts *first* otherwise they
       # might not be available yet to other templates as {{ site.posts }}
-      if entries.include?('_posts')
-        entries.delete('_posts')
+      if directories.include?('_posts')
+        directories.delete('_posts')
         read_posts(dir)
       end
-
-      entries.each do |f|
-        if File.directory?(File.join(base, f))
-          next if self.dest.sub(/\/$/, '') == File.join(base, f)
-          transform_pages(File.join(dir, f))
-        else
-          first3 = File.open(File.join(self.source, dir, f)) { |fd| fd.read(3) }
-
-          if first3 == "---"
-            # file appears to have a YAML header so process it as a page
-            page = Page.new(self.source, dir, f)
-            page.render(self.layouts, site_payload)
-            page.write(self.dest)
+      
+      [directories, files].each do |entries|
+        entries.each do |f|
+          if File.directory?(File.join(base, f))
+            next if self.dest.sub(/\/$/, '') == File.join(base, f)
+            transform_pages(File.join(dir, f))
+          elsif Pager.pagination_enabled?(self.config, f)
+            paginate_posts(f, dir)
           else
-            # otherwise copy the file without transforming it
-            FileUtils.mkdir_p(File.join(self.dest, dir))
-            FileUtils.cp(File.join(self.source, dir, f), File.join(self.dest, dir, f))
+            first3 = File.open(File.join(self.source, dir, f)) { |fd| fd.read(3) }
+
+            if first3 == "---"
+              # file appears to have a YAML header so process it as a page
+              page = Page.new(self, self.source, dir, f)
+              page.render(self.layouts, site_payload)
+              page.write(self.dest)
+            else
+              # otherwise copy the file without transforming it
+              FileUtils.mkdir_p(File.join(self.dest, dir))
+              FileUtils.cp(File.join(self.source, dir, f), File.join(self.dest, dir, f))
+            end
           end
         end
       end
@@ -146,16 +238,48 @@ module Jekyll
     #
     # Returns {"site" => {"time" => <Time>,
     #                     "posts" => [<Post>],
-    #                     "categories" => [<Post>],
-    #                     "topics" => [<Post>] }}
+    #                     "categories" => [<Post>]}
     def site_payload
-      {"site" => {
-        "time" => Time.now, 
-        "posts" => self.posts.sort { |a,b| b <=> a },
-        "categories" => post_attr_hash('categories'),
-        "topics" => post_attr_hash('topics')
-      }}
+      {"site" => self.config.merge({
+          "time"       => Time.now,
+          "posts"      => self.posts.sort { |a,b| b <=> a },
+          "categories" => post_attr_hash('categories'),
+          "tags"       => post_attr_hash('tags')})}
+    end
+
+    # Filter out any files/directories that are hidden or backup files (start
+    # with "." or "#" or end with "~") or contain site content (start with "_")
+    # unless they are "_posts" directories or web server files such as
+    # '.htaccess'
+    def filter_entries(entries)
+      entries = entries.reject do |e|
+        unless ['_posts', '.htaccess'].include?(e)
+          ['.', '_', '#'].include?(e[0..0]) || e[-1..-1] == '~' || self.exclude.include?(e)
+        end
+      end
+    end
+
+    # Paginates the blog's posts. Renders the index.html file into paginated directories, ie: page2, page3...
+    # and adds more wite-wide data
+    #
+    # {"paginator" => { "page" => <Number>,
+    #                   "per_page" => <Number>,
+    #                   "posts" => [<Post>],
+    #                   "total_posts" => <Number>,
+    #                   "total_pages" => <Number>,
+    #                   "previous_page" => <Number>,
+    #                   "next_page" => <Number> }}
+    def paginate_posts(file, dir)
+      all_posts = self.posts.sort { |a,b| b <=> a }
+      pages = Pager.calculate_pages(all_posts, self.config['paginate'].to_i)
+      pages += 1
+      (1..pages).each do |num_page|
+        pager = Pager.new(self.config, num_page, all_posts, pages)
+        page = Page.new(self, self.source, dir, file)
+        page.render(self.layouts, site_payload.merge({'paginator' => pager.to_hash}))
+        suffix = "page#{num_page}" if num_page > 1
+        page.write(self.dest, suffix)
+      end
     end
   end
-
 end
